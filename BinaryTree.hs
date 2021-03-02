8075a826aac37190c497b4195f75a6804e8f8b0a
Florian <wuermseer.florian@gmail.com>
2021-03-02 21:21:18 +0100

    Added invert function for binary tree

--- BinaryTree.hs
+++ BinaryTree.hs
@@ -1,24 +1,28 @@
-import LinkedList ( LinkedList(..), inverseFold )
+import LinkedList
 
-data BinaryTree a = Node (BinaryTree a) a (BinaryTree a) | End
+data BinaryTree a = Node (BinaryTree a) a (BinaryTree a) | Tip
     deriving (Show)
 
 newTree :: a -> BinaryTree a
-newTree x = Node End x End
+newTree x = Node Tip x Tip
 
 depth :: Ord a => BinaryTree a -> Integer
-depth End = 0
+depth Tip = 0
 depth (Node left value right) = (depth left + 1) `max` (depth right + 1)
 
 -- Adding value in order and without duplicates to allow searching and sorting
 addToSorted :: Ord a => a -> BinaryTree a -> BinaryTree a
-addToSorted x End = newTree x
+addToSorted x Tip = newTree x
 addToSorted x tree@(Node left y right)
     | x < y = Node (addToSorted x left) y right
     | x > y = Node left y (addToSorted x right)
     | otherwise = Node left y right
 
 llToBST :: Ord a => LinkedList a -> BinaryTree a -> BinaryTree a
-llToBST Nil _ = End
+llToBST Nil _ = Tip
 llToBST (Head y Nil) _ = newTree y
 llToBST xs tree = inverseFold addToSorted tree xs
+
+invertBT :: BinaryTree a -> BinaryTree a
+invertBT Tip = Tip
+invertBT (Node left y right) = Node (invertBT right) y (invertBT left)
